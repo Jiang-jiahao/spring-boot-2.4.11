571bb58e4f2d38789081254effd7ee4124ec8486
jiangjiahao <Wohenwuqu123>
2022-04-02 16:53:22 +0800

    初始化源码

---
+++ springbootdemo/src/main/java/com/jjh/JjhApplication.java
@@ -0,0 +1,11 @@
+package com.jjh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjhApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JjhApplication.class);
+	}
+}
